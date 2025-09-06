89d723b677b1ebbfa714c46f334b04b8b122d2d7
niranjan <niranjan64461231@gmail.com>
2025-09-06 20:43:43 +0530

    c

--- main.js
+++ main.js
@@ -90,7 +90,29 @@ quizbtn1.addEventListener("click", () => {
   QuizCards();
 });
 
+let navigationbox = document.createElement("div");
+navigationbox.classList.add(
+  "w-[10%]",
+  "xs:w-[13%]",
+  "flex",
+  "flex-col",
+  "items-center",
+  "pt-10"
+);
 let navigationContainer = document.createElement("div");
+navigationContainer.classList.add(
+  "w-[60%]",
+  "xs:h-80",
+  "h-[90%]",
+  "grid",
+  "xs:grid-cols-2",
+  "grid-cols-1",
+  "items-center",
+  "justify-center",
+  "text-[#6B4F44]",
+  // "mx-1"
+  "gap-x-1"
+);
 // let score = 0;
 let userAnswers = {};
 let selectedQuestionIndexs = [];
@@ -101,62 +123,60 @@ async function QuizPage(topic) {
   let selectedTopic = data.find((obj) => obj.topic === topic);
   // console.log(selectedTopic);
   Questions = selectedTopic.questions;
-  console.log(Questions);
+  // console.log(Questions);
   while (selectedQuestionIndexs.length < 10) {
     let randomIndex = Math.floor(Math.random() * Questions.length);
     if (!selectedQuestionIndexs.includes(randomIndex)) {
       selectedQuestionIndexs.push(randomIndex);
     }
   }
-  console.log(selectedQuestionIndexs);
+  // console.log(selectedQuestionIndexs);
   maincontainer.innerHTML = "";
   maincontainer.classList.remove("flex-col");
   maincontainer.classList.add("flex-row");
 
-  navigationContainer.classList.add(
-    "w-[10%]",
-    "h-full",
-    "flex",
-    "flex-col",
-    "items-center",
-    "justify-center",
-    "text-[#6B4F44]",
-    "mx-1"
-  );
   let questionContainer = document.createElement("div");
   questionContainer.classList.add(
-    "w-[90%]",
-    "h-[full]",
+    "md:w-[60%]",
+    "w-[90]",
+    "h-full",
     // "bg-white",
     "flex",
     "flex-col",
     "gap-3",
-    "p-20"
+    "pl-20",
+    "pt-20"
   );
-  maincontainer.appendChild(navigationContainer);
+  navigationbox.appendChild(navigationContainer);
+  maincontainer.appendChild(navigationbox);
   // let rulescontainer=document.createElement("div");
-  let ruleHeading=document.createElement("h1");
-  ruleHeading.classList.add("text-2xl","font-serif","font-semibold","text-black");
-  ruleHeading.innerText="Rules to follow:";
-  console.log(ruleHeading);
-  questionContainer.appendChild(ruleHeading)
-  let rulesblock=document.createElement("div");
-  rulesblock.classList.add("flex","flex-col","gap-3")
-  let rules= [
-  "Read all questions and options carefully.",
-  "Answer based on your best knowledge; don't rush.",
-  "If unsure, choose your best guess.",
-  "Please use buttons to navigate Questions.",
-  "Please Don't perss Submit before compleating.",
-  "Complete the quiz honestly without external help."
-];
-rules.map((val,ind)=>{
-  let rule=document.createElement("p");
-  rule.classList.add("text-lg","px-2","py-2","font-serif")
-  rule.innerText=`${ind+1}. ${val}`;
-  rulesblock.appendChild(rule)
-})
-questionContainer.appendChild(rulesblock)
+  let ruleHeading = document.createElement("h1");
+  ruleHeading.classList.add(
+    "text-2xl",
+    "font-serif",
+    "font-semibold",
+    "text-black"
+  );
+  ruleHeading.innerText = "Rules to follow:";
+  // console.log(ruleHeading);
+  questionContainer.appendChild(ruleHeading);
+  let rulesblock = document.createElement("div");
+  rulesblock.classList.add("flex", "flex-col", "gap-3");
+  let rules = [
+    "Read all questions and options carefully.",
+    "Answer based on your best knowledge; don't rush.",
+    "If unsure, choose your best guess.",
+    "Please use buttons to navigate Questions.",
+    "Please Don't perss Submit before compleating.",
+    "Complete the quiz honestly without external help.",
+  ];
+  rules.map((val, ind) => {
+    let rule = document.createElement("p");
+    rule.classList.add("text-lg", "px-2", "py-2", "font-serif");
+    rule.innerText = `${ind + 1}. ${val}`;
+    rulesblock.appendChild(rule);
+  });
+  questionContainer.appendChild(rulesblock);
   maincontainer.appendChild(questionContainer);
   for (let i = 0; i < 10; i++) {
     let navButton = document.createElement("div");
@@ -174,10 +194,12 @@ questionContainer.appendChild(rulesblock)
       "cursor-pointer"
     );
     navButton.addEventListener("click", () => {
-      currentQuestionIndex = i;
       questionContainer.innerHTML = "";
       Array.from(navigationContainer.children).map((btn, btnindex) => {
-        if (btnindex != 10) {
+        if (
+          btnindex != 10 &&
+          !Object.keys(userAnswers).includes(String(btnindex))
+        ) {
           btn.classList.remove("bg-gray-500", "text-white");
           btn.classList.add("bg-white");
         }
@@ -187,13 +209,12 @@ questionContainer.appendChild(rulesblock)
       loadQuestion(i);
     });
     navigationContainer.appendChild(navButton);
-    
   }
 
-  let navButton = document.createElement("button");
-  navButton.innerText = "Submit";
-  navButton.classList.add(
-    "w-min-content",
+  let submitbtn = document.createElement("button");
+  submitbtn.innerText = "Submit";
+  submitbtn.classList.add(
+    "w-max-content",
     "h-10",
     "m-2",
     "px-2",
@@ -202,23 +223,24 @@ questionContainer.appendChild(rulesblock)
     "bg-[#6B4F44]",
     "text-white"
   );
-  navButton.innerText = "Submit";
-  navigationContainer.appendChild(navButton);
+  submitbtn.innerText = "Submit";
+  navigationbox.appendChild(submitbtn);
   // let nav = navigationContainer.children;
-navButton.addEventListener("click", () => {
-  console.log(userAnswers);
-  let score=0;
-  Object.keys(userAnswers).map((val)=>{
-    console.log(Questions[selectedQuestionIndexs[val]]["question"]);
-    console.log("q:",selectedQuestionIndexs[val],"uo:",userAnswers[val],"Ao",Questions[selectedQuestionIndexs[val]]["answer"]);
-    if(Questions[selectedQuestionIndexs[val]]["answer"]==userAnswers[val])
-    {
-      score++
-    }
-  })
-  ResultPage(score);
-  console.log("final score:",score);
-});
+  submitbtn.addEventListener("click", () => {
+    // console.log(userAnswers);
+    let score = 0;
+    Object.keys(userAnswers).map((val) => {
+      // console.log(Questions[selectedQuestionIndexs[val]]["question"]);
+      // console.log("q:",selectedQuestionIndexs[val],"uo:",userAnswers[val],"Ao",Questions[selectedQuestionIndexs[val]]["answer"]);
+      if (
+        Questions[selectedQuestionIndexs[val]]["answer"] == userAnswers[val]
+      ) {
+        score++;
+      }
+    });
+    ResultPage(score);
+    // console.log("final score:",score);
+  });
   function loadQuestion(index) {
     let questionblock = document.createElement("h1");
     questionblock.classList.add(
@@ -237,45 +259,83 @@ navButton.addEventListener("click", () => {
     optionsContainer.classList.add("flex", "flex-col", "gap-3", "w-full");
     let options = Questions[selectedQuestionIndexs[index]]["options"];
     options.forEach((option, optionIndex) => {
-      let optionElement = document.createElement("div");
-      optionElement.classList.add(
-        "border",
-        "px-5","py-2",
+      let optionbox = document.createElement("div");
+      optionbox.classList.add(
+        "flex",
+        "flex-row",
+        "gap-3",
+        "items-center",
+        "justify-start",
+        "bg-white",
+        "w-[60%]",
+        "md:w-[40%]",
         "rounded-lg",
         "cursor-pointer",
+        "border",
         "bg-white",
         "hover:bg-gray-200",
-        "w-[40%]"
+        "pl-5"
       );
+      let radio = document.createElement("div");
+      radio.classList.add(
+        "flex",
+        "items-center",
+        "justify-center",
+        "w-5",
+        "h-5",
+        "border",
+        // "hover:border-white",
+        "border-black",
+      );
+      optionbox.appendChild(radio);
+      let optionElement = document.createElement("div");
+      optionElement.classList.add("px-5", "py-2");
       optionElement.innerText = option;
       if (userAnswers[index] === optionIndex) {
-        optionElement.classList.add("bg-green-300", "text-white");
+        optionbox.classList.add("bg-green-500", "text-white");
+        optionbox.children[0].innerHTML=`<i class="fa-solid fa-check text-black"></i>`;
       }
-      optionElement.addEventListener("click", () => {
+      optionbox.addEventListener("click", () => {
         userAnswers[index] = optionIndex;
         Array.from(optionsContainer.children).forEach((child) => {
-          child.classList.remove("bg-green-300", "text-white");
+          child.classList.remove("bg-green-500", "text-white");
+          child.children[0].classList.remove("bg-white");
+          child.children[0].innerHTML="";
         });
-        optionElement.classList.add("bg-green-300", "text-white");
-        console.log("starting", index);
-
+        // console.log("starting", index);
         let nav = Array.from(navigationContainer.children);
         Object.keys(userAnswers).map((val) => {
-          console.log("changing color");
           nav[val].classList.remove("bg-white");
           nav[val].classList.remove("bg-gray-500");
-          nav[val].classList.add("bg-green-300");
+          nav[val].classList.add("bg-green-500");
         });
-        console.log(userAnswers);
+        optionbox.classList.add("bg-green-500", "text-white");
+        optionbox.children[0].classList.add("bg-white");
+        optionbox.children[0].innerHTML=`<i class="fa-solid fa-check text-black"></i>`;
+
+
+        // console.log(userAnswers);
       });
-      optionsContainer.appendChild(optionElement);
+      optionbox.appendChild(optionElement);
+      optionsContainer.appendChild(optionbox);
     });
     questionContainer.appendChild(optionsContainer);
   }
+
+  //details
+  let tracking = document.createElement("div");
+  tracking.classList.add(
+    "w-[25%]",
+    "bg-gray-500",
+    "hidden",
+    "md:flex",
+    "md:flex-col",
+    "gap-3"
+  );
+  tracking.innerText = "tracking";
+  maincontainer.appendChild(tracking);
 }
 
-
-
-function ResultPage(score){
-maincontainer.innerText=score;
+function ResultPage(score) {
+  maincontainer.innerText = score;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html","./*.js"],
+  content: ["./*.html","./main.js"],
   theme: {
     extend: {
       screens:{
+        'xs':'550px',
         '5xl':'2300px',
       },
     },
